d690cf4a75bdc5232b87ebf9d1c03e1eb404e520
AWBCoding <a.williamsbhatti@gmail.com>
2019-09-27 22:33:07 +0100

    GRID MAP WITH OBSTACLES
    Need to figure out how to make sure randomness occurs during run time.

---
+++ AIFG1/AIFG/Assets/Scripts/GeneratingLevel.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratingLevel : MonoBehaviour
+{
+
+    List<locationscd> thefullloc;
+    Queue<locationscd> mixedlocations;
+    locationscd playerspawnpoint;
+    public string fulllevel = "Complete Level";
+    public Transform prefgrid;
+    public Transform[] hurdles;
+    public Transform hurdlepref;
+    public Vector2 gridscale;
+    [Range(0, 5)]
+    public float borders;
+    public int index = 15;
+    int randomh; 
+    
+
+    [Range(0, 1)]
+    public float hurdlepercent;
+
+    public void LevelCreation()
+    {
+      
+
+        if (transform.Find(fulllevel))
+        {
+            DestroyImmediate(transform.Find(fulllevel).gameObject);
+        }
+
+        thefullloc = new List<locationscd>();
+        for (int xaxis = 0; xaxis < gridscale.x; xaxis++)
+        {
+            for (int yaxis = 0; yaxis < gridscale.y; yaxis++)
+            {
+                thefullloc.Add(new locationscd(xaxis, yaxis));
+            }
+
+        }
+
+
+        mixedlocations = new Queue<locationscd>(Shuffling.Fisher(thefullloc.ToArray(), index));
+        playerspawnpoint = new locationscd((int)gridscale.x /2, (int)gridscale.y /2);
+
+
+
+        Transform thecompleted = new GameObject(fulllevel).transform;
+        thecompleted.parent = transform;
+
+        for (int xaxis = 0; xaxis<gridscale.x; xaxis++)
+        {
+            for (int yaxis = 0; yaxis < gridscale.y; yaxis++)
+            {
+                Vector3 SectionLocation = locationtopos(xaxis, yaxis);
+                Transform thegridsection = Instantiate(prefgrid, SectionLocation, Quaternion.Euler(Vector3.right * 90)) as Transform;
+                thegridsection.parent = thecompleted;
+                thegridsection.localScale = Vector3.one * (1 - borders);
+
+            }
+        }
+
+        bool[,] hurdlelocations = new bool[(int)gridscale.x, (int)gridscale.y];
+        int numberofhurdle = (int)(gridscale.x * gridscale.y * hurdlepercent);
+        int howmany = 0;
+        for(int check = 0; check<numberofhurdle; check++)
+        {
+            //randomh = Random.Range(0, 2);
+            //for(int i=0; i<3; i++)
+            //{
+            //    if(randomh == i)
+            //    {
+            //        hurdlepref = hurdles[i];
+            //    }
+            //}
+            locationscd rndlocation = atrandomloc();
+            hurdlelocations[rndlocation.x, rndlocation.y] = true;
+            howmany++;
+
+            if (rndlocation != playerspawnpoint)
+            {
+                Vector3 hurdleloc = locationtopos(rndlocation.x, rndlocation.y);
+                Transform newhurdle = Instantiate(hurdlepref, hurdleloc + Vector3.up * 0.50f, Quaternion.identity) as Transform;
+                newhurdle.parent = thecompleted;
+            }
+
+            else
+            {
+                hurdlelocations[rndlocation.x, rndlocation.y] = false;
+                howmany--;
+
+
+            }
+
+
+        }
+
+
+    }
+
+    public void Start()
+    {
+        LevelCreation();
+        hurdlepercent = Random.Range(0.1f, 0.26f);
+        index = Random.Range(50, 57);
+    }
+
+
+ 
+
+
+
+    Vector3 locationtopos(int x, int y)
+    {
+        return new Vector3(-gridscale.x / 2 + 0.50f + x, 0, -gridscale.y + 0.50f + y);
+    }
+
+    public struct locationscd
+    {
+        public int x;
+        public int y;
+
+        public locationscd(int varx, int vary)
+        {
+            x = varx;
+            y = vary;
+        }
+
+
+        public static bool operator ==(locationscd parta, locationscd part2)
+        {
+            return parta.x == part2.x && parta.y == part2.y;
+        }
+
+        public static bool operator !=(locationscd parta, locationscd part2)
+        {
+            return !(parta == part2);
+        }
+
+
+    }
+
+         
+
+    public locationscd atrandomloc()
+    {
+        locationscd randloc = mixedlocations.Dequeue();
+        mixedlocations.Enqueue(randloc);
+        return randloc;
+    }
+
+}
